b0279e6305862394cf79a1a29b2c9b2c711c6286
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2017-08-04 01:10:39 -0300

    criando protótipo de teste da entidade queixa

---
+++ src/test/java/com/ufcg/si1/model/TestQueixa.java
@@ -0,0 +1,30 @@
+package com.ufcg.si1.model;
+
+import com.ufcg.si1.model.Queixa;
+import org.junit.*;
+
+/**
+ * Teste para a entidade Queixa
+ * 
+ * @author Tiago
+ *
+ */
+public class TestQueixa {
+
+	private Queixa queixa;
+	
+	@Before
+	public void setup() {
+		
+	}
+	
+	@Test
+	public void abrir() {
+		
+	}
+	
+	@Test
+	public void fechar() {
+		
+	}
+}
