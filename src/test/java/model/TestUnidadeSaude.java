920425aacde1a0e6a6a2c6c4019879e1d1d62fe6
Mariana Mendes <marianamendes@MacBook-Pro-de-Joao.local>
2017-08-04 20:46:31 -0300

    criando classe de teste para unidade de saude

---
+++ src/test/java/model/TestUnidadeSaude.java
@@ -0,0 +1,17 @@
+package model;
+
+import com.ufcg.si1.model.UnidadeSaude;
+import org.junit.*;
+
+public class TestUnidadeSaude {
+	
+	
+	private UnidadeSaude unidade;
+	
+	
+	@Before
+	public void setUp() {
+		
+	}
+
+}
