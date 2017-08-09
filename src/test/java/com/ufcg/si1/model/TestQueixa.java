f2bcc76b2348425059ef153435caee0183c29adb
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2017-08-09 12:13:11 -0300

    modificando os testes pra ficar de acordo com as mudanças na entidade Queixa

--- src/test/java/com/ufcg/si1/model/TestQueixa.java
+++ src/test/java/com/ufcg/si1/model/TestQueixa.java
@@ -1,6 +1,7 @@
 package com.ufcg.si1.model;
 
 import com.ufcg.si1.model.queixa.Queixa;
+import com.ufcg.si1.model.queixa.STATUS_QUEIXA;
 
 import exceptions.ObjetoInvalidoException;
 
@@ -18,25 +19,29 @@ public class TestQueixa {
 	private Queixa queixa2;
 	private Queixa queixa3;
 	
+	private Pessoa taigo;
+	private Pessoa ednaldo;
+	private Pessoa priscyla;
+	
 	@Before
 	public void setup() {
-		queixa1 = new Queixa(5, "Comi uma teju e vomitei", 1, "",
-                "Taigo", "taigo@gmailcom",
+		taigo = new Pessoa("Taigo", "taigo@gmailcom",
 				  "Idelfonso Aires", "Paraiba", "Campina Grande");
-		queixa2 = new Queixa(6, "Bati um lampião de gás na cabeça", 2, "",
-                "Ednaldo Pereira", "ednaldo@gmailcom",
+		ednaldo = new Pessoa("Ednaldo Pereira", "ednaldo@gmailcom",
 				  "Cachoeira dos Guedes", "Paraiba", "Guarabira");
-		queixa3 = new Queixa(7, "Tomei muitos ansiolíticos de uma vez", 3, "",
-                "Priscyla", "pri@gmailcom",
+		priscyla = new Pessoa("Priscyla", "pri@gmailcom",
 				  "Rua João Maurício", "Paraiba", "João Pessoa");
+		queixa1 = new Queixa(5, "Comi uma teju e vomitei", 1, "", taigo);
+		queixa2 = new Queixa(6, "Bati um lampião de gás na cabeça", 2, "", ednaldo);
+		queixa3 = new Queixa(7, "Tomei muitos ansiolíticos de uma vez", 3, "", priscyla);
 	}
 		
 	@Test
 	public void abrir() {
 		try {
-			Assert.assertEquals(3, queixa3.getSituacao());
+			Assert.assertEquals(STATUS_QUEIXA.FECHADA, queixa3.getSituacao());
 			queixa3.abrir();
-			Assert.assertEquals(1, queixa3.getSituacao());
+			Assert.assertEquals(STATUS_QUEIXA.ABERTA, queixa3.getSituacao());
 		} catch (ObjetoInvalidoException e) {
 			e.printStackTrace();
 		}
@@ -54,13 +59,13 @@ public class TestQueixa {
 	@Test
 	public void fechar() {
 		try {
-			Assert.assertEquals(1, queixa1.getSituacao());
+			Assert.assertEquals(STATUS_QUEIXA.ABERTA, queixa1.getSituacao());
 			queixa1.fechar("Passou");
-			Assert.assertEquals(3, queixa1.getSituacao());
+			Assert.assertEquals(STATUS_QUEIXA.FECHADA, queixa1.getSituacao());
 			
-			Assert.assertEquals(2, queixa2.getSituacao());
+			Assert.assertEquals(STATUS_QUEIXA.EM_ANDAMENTO, queixa2.getSituacao());
 			queixa2.fechar("Nunca não");
-			Assert.assertEquals(3, queixa2.getSituacao());
+			Assert.assertEquals(STATUS_QUEIXA.FECHADA, queixa2.getSituacao());
 		} catch (ObjetoInvalidoException e) {
 			e.printStackTrace();
 		}
