8974c6813eddb0f20fd2db0e6e9430c85cd5ad94
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2017-08-17 23:00:51 -0300

    Criando especialidadeRest

---
+++ src/main/java/com/ufcg/si1/repositories/EspecialidadeRepository.java
@@ -0,0 +1,9 @@
+package com.ufcg.si1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ufcg.si1.model.Especialidade;
+
+public interface EspecialidadeRepository extends JpaRepository<Especialidade,  Long> {
+
+}
