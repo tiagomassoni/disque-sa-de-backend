29f52ec7ea1007cc9792da9b47bf7fe6ffcefbf7
Joao Mauricio <joao.carvalho@ccc.ufcg.edu.br>
2017-08-17 23:07:45 -0300

    Implementando metodo de getSituacaoQueixa

--- src/main/java/com/ufcg/si1/controller/prefeitura/SituacaoNormal.java
+++ src/main/java/com/ufcg/si1/controller/prefeitura/SituacaoNormal.java
@@ -3,9 +3,9 @@ package com.ufcg.si1.controller.prefeitura;
 public class SituacaoNormal implements SituacaoPrefeitura {
 
 	@Override
+	// Vai ser uma string mesmo?
 	public String getSituacaoQueixa(int queixasAbertas, int totalQueixas) {
-		// TODO Auto-generated method stub
-		return null;
+		return (double) queixasAbertas / totalQueixas > 0.2 ? "ruim":"regular";
 	}
 
 }
