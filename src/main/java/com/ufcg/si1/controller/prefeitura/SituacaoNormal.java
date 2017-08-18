bd1d5f04ad1eb29ae952bdac501449db38e87c7f
Joao Mauricio <joao.carvalho@ccc.ufcg.edu.br>
2017-08-17 22:52:36 -0300

    Criando situacao da prefeitura

---
+++ src/main/java/com/ufcg/si1/controller/prefeitura/SituacaoNormal.java
@@ -0,0 +1,11 @@
+package com.ufcg.si1.controller.prefeitura;
+
+public class SituacaoNormal implements SituacaoPrefeitura {
+
+	@Override
+	public String getSituacaoQueixa(int queixasAbertas, int totalQueixas) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/ufcg/si1/controller/prefeitura/SituacaoPrefeitura.java
@@ -0,0 +1,6 @@
+package com.ufcg.si1.controller.prefeitura;
+
+public interface SituacaoPrefeitura {
+	
+	String getSituacaoQueixa(int queixasAbertas, int totalQueixas);
+}
